3d86bd2a389aa5ec104b4960b0a34911074dfa71
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-07-23 19:20:06 +0300

    add profileComponent

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import Button from "../../components/button/Button";
 
-const Profile = () => {
+export const Profile = React.memo((props: ProfilePropsType) => {
+
+    const {name, avatar, cardsCount, email, logout} = props;
+
+    const logoutHandler = () => {
+
+    }
+
     return (
-        <div>
-            profile
+        <div className={"profile"}>
+            <h2>Personal Information</h2>
+            <div className={"profile__avatar"}>
+                <img src={avatar} alt="avatar"/>
+            </div>
+            <div className={"profile__name"}>
+                <h3>{name}</h3>
+            </div>
+            <div className={"profile__email"}>
+                <h4>{email}</h4>
+            </div>
+            <div className={"profile__cardsCount"}>
+                <h4>{cardsCount}</h4>
+            </div>
+            <div className={"profile__button"}>
+                <Button
+                    title={"logout"}
+                    type={"button"}
+                    disabled={false}
+                    callBack={logoutHandler}
+                />
+            </div>
         </div>
     );
-};
+});
 
-export default Profile;
+type ProfilePropsType = {
+    avatar: string
+    name: string
+    email: string
+    cardsCount: number
+    logout: () => void
+}
