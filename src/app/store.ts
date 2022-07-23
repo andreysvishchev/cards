e367149ac7110d7bf5d094c7285401c453839d22
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-07-23 18:07:58 +0300

    minor

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,16 @@
-import { AnyAction, applyMiddleware, combineReducers, legacy_createStore as createStore } from "redux";
-import thunk, { ThunkDispatch } from 'redux-thunk'
-import { TypedUseSelectorHook, useSelector } from "react-redux";
-import { loginReducer } from "../pages/login/loginReducer";
-import { cardsReducer } from "../pages/cards/cardsReducer";
-import { registrationReducer } from "../pages/registration/registrationReducer";
-import { appReducer } from "./appReducer";
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
+import thunk, {ThunkDispatch} from 'redux-thunk'
+import {TypedUseSelectorHook, useSelector} from "react-redux";
+import {loginReducer} from "../pages/login/loginReducer";
+import {cardsReducer} from "../pages/cards/cardsReducer";
+import {registrationReducer} from "../pages/registration/registrationReducer";
+import {appReducer} from "./appReducer";
 
 const rootReducer = combineReducers({
-  app: appReducer,
-  login: loginReducer,
-  cards: cardsReducer,
-  registration: registrationReducer
+    app: appReducer,
+    login: loginReducer,
+    cards: cardsReducer,
+    registration: registrationReducer
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
