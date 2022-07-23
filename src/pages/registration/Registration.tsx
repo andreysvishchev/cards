a7154b9946707b4421a260a58f37861aa7d0d7a0
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-07-23 18:29:36 +0300

    use custom hooks

--- src/pages/registration/Registration.tsx
+++ src/pages/registration/Registration.tsx
@@ -1,91 +1,90 @@
-import { useFormik } from 'formik';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import { RequestStatusType } from '../../app/appReducer';
-import { AppDispatchType, useAppSelector } from '../../app/store';
+import {useFormik} from 'formik';
+import {useState} from 'react';
+import {NavLink} from 'react-router-dom';
 import Button from '../../components/button/Button';
 import Input from '../../components/input/Input';
-import { changeStatusRegistration, sendRegistrationData } from './registrationReducer';
+import {sendRegistrationData} from './registrationReducer';
+import {useAppDispatch, useAppSelector} from "../../hooks/hooks";
 
 type FormikErrorType = {
-  email?: string
-  password?: string
-  confirm_password?: string
+    email?: string
+    password?: string
+    confirm_password?: string
 }
 
 const Registration = () => {
 
-  const send = useAppSelector<boolean>(state => state.registration.send)
-  const dispatch = useDispatch<AppDispatchType>()
-  const [messageEmail, setMessageEmail] = useState<string>('example@mail.com')
-  const status = useAppSelector<RequestStatusType>(state => state.app.status)
+    const send = useAppSelector(state => state.registration.send)
+    const dispatch = useAppDispatch()
+    const [messageEmail, setMessageEmail] = useState<string>('example@mail.com')
+    const status = useAppSelector(state => state.app.status)
 
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-      confirm_password: ''
-    },
-    validate: (values) => {
-      const errors: FormikErrorType = {};
-      if (!values.email) {
-        errors.email = 'Поле обязательно для заполнения';
-      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Email указан некорректно';
-      }
-      if (values.password !== values.confirm_password) {
-        errors.confirm_password = 'Пароли должны совпадать'
-      }
-      if (!values.password) {
-        errors.password = 'Поле обязательно для заполнения';
-      }
-      if (!values.confirm_password) {
-        errors.confirm_password = 'Поле обязательно для заполнения';
-      }
-      return errors;
-    },
-    onSubmit: values => {
-      const data = {
-        email: values.email,
-        password: values.password
-      }
-      dispatch(sendRegistrationData(data))
-      setMessageEmail(values.email)
-      formik.resetForm()
-    },
-  })
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            confirm_password: ''
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {};
+            if (!values.email) {
+                errors.email = 'Поле обязательно для заполнения';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Email указан некорректно';
+            }
+            if (values.password !== values.confirm_password) {
+                errors.confirm_password = 'Пароли должны совпадать'
+            }
+            if (!values.password) {
+                errors.password = 'Поле обязательно для заполнения';
+            }
+            if (!values.confirm_password) {
+                errors.confirm_password = 'Поле обязательно для заполнения';
+            }
+            return errors;
+        },
+        onSubmit: values => {
+            const data = {
+                email: values.email,
+                password: values.password
+            }
+            dispatch(sendRegistrationData(data))
+            setMessageEmail(values.email)
+            formik.resetForm()
+        },
+    })
 
-  return (
-    <div className='frame'>
-      <div className='title'>{send ? 'Check Email' : 'Sign Up'}</div>
-      {send ?
-        <div className='registration'>
-          <div className='registration__img'></div>
+    return (
+        <div className='frame'>
+            <div className='title'>{send ? 'Check Email' : 'Sign Up'}</div>
+            {send ?
+                <div className='registration'>
+                    <div className='registration__img'></div>
+                </div>
+                :
+                <form className="form" onSubmit={formik.handleSubmit}>
+                    <Input placeholder='Email'
+                           {...formik.getFieldProps('email')}
+                           error={formik.errors.email && formik.touched.email}
+                           errorText={formik.errors.email}/>
+                    <Input placeholder='Password' password={true} {...formik.getFieldProps('password')}
+                           error={formik.errors.password && formik.touched.password}
+                           errorText={formik.errors.password}/>
+                    <Input placeholder='Confirm password' password={true} {...formik.getFieldProps('confirm_password')}
+                           error={formik.errors.confirm_password && formik.touched.confirm_password}
+                           errorText={formik.errors.confirm_password}/>
+                    <div style={{marginTop: '36px', display: 'flex', justifyContent: 'center'}}>
+                        <Button title='Sign Up' disabled={status === 'loading'} type='submit'/>
+                    </div>
+                </form>
+            }
+            <div className="registration__text">
+                {send ? `We’ve sent an Email with instructions to ${messageEmail}` : 'Don’t have an account?'}
+            </div>
+            <NavLink className={send ? 'button button--registration' : 'registration__link'}
+                     to="/">{send ? 'Back to login' : 'Sign In'}</NavLink>
         </div>
-        :
-        <form className="form" onSubmit={formik.handleSubmit}>
-          <Input placeholder='Email'
-            {...formik.getFieldProps('email')}
-            error={formik.errors.email && formik.touched.email}
-            errorText={formik.errors.email} />
-          <Input placeholder='Password' password={true} {...formik.getFieldProps('password')}
-            error={formik.errors.password && formik.touched.password}
-            errorText={formik.errors.password} />
-          <Input placeholder='Confirm password' password={true} {...formik.getFieldProps('confirm_password')}
-            error={formik.errors.confirm_password && formik.touched.confirm_password}
-            errorText={formik.errors.confirm_password} />
-          <div style={{ marginTop: '36px', display: 'flex', justifyContent: 'center' }}>
-            <Button title='Sign Up' disabled={status === 'loading'} type='submit' />
-          </div>
-        </form>
-      }
-      <div className="registration__text">
-        {send ? `We’ve sent an Email with instructions to ${messageEmail}` : 'Don’t have an account?'}
-      </div>
-      <NavLink className={send ? 'button button--registration' : 'registration__link'} to="/">{send ? 'Back to login' : 'Sign In'}</NavLink>
-    </div>
-  );
+    );
 };
 
 export default Registration;
