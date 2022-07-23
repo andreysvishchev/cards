f9c6287019dc1f0ffcfa77e9e5d5806899a46b75
Andrey Svishchev <andrey.svishchev@yandex.ru>
2022-07-23 15:16:03 +0300

    instal formik

--- src/pages/registration/Registration.tsx
+++ src/pages/registration/Registration.tsx
@@ -1,29 +1,18 @@
 import React from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { Link, NavLink } from 'react-router-dom';
 import Button from '../../components/button/Button';
 import Input from '../../components/input/Input';
 
-interface formFields {
-  email: string
-  password: string
-  confirmPassword: string
-}
 
 const Registration = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<formFields>();
-  const onSubmit: SubmitHandler<formFields> = (data) => {
-    console.log(data);
 
-  }
   return (
     <div className='frame'>
       <div className='title'>Sign Up</div>
-      <form className="form" onSubmit={handleSubmit(onSubmit)}>
-        <Input placeholder='Email' {...register("email", {})} />
-        <div>
-          {errors?.email && <p>{errors?.email.message}</p>}
-        </div>
+      <form className="form" >
+
+        <Input placeholder='Email' />
         <Input placeholder='Password' password={true} />
         <Input placeholder='Confirm password' password={true} />
         <div style={{ marginTop: '36px', display: 'flex', justifyContent: 'center' }}>
