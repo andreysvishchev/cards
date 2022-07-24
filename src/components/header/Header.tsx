88a0ab0b9952a78f0aa8234acb0cab3ade52bbde
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-07-24 16:42:16 +0300

    header logo changes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,37 @@
 import Button from "../button/Button"
 import logo from '../../img/logo.svg'
+import React from "react";
 
 
-export const Header = () => {
-  return (
-    <header className="header">
-      <div className="container container--header">
-        <a href="#" className="logo">
-          <img src={logo} alt="Logo" />
-        </a>
-        <Button title='Sign in' />
-      </div>
-    </header>
-  )
+export const Header: React.FC<HeaderPropsType> = (props) => {
+
+    const {isAuth, userName, avatar} = props
+
+    return (
+        <header className="header">
+            <div className="container container--header">
+                <a href="#" className="logo">
+                    <img src={logo} alt="Logo"/>
+                </a>
+                {isAuth
+                    ? <div className={'user-data'}>
+                        <div className={'user-data__name'}>
+                            {userName}
+                        </div>
+                        <div className={'user-data__avatar'}>
+                            <img src={avatar} alt=""/>
+                        </div>
+                    </div>
+                    : <Button title='Sign in'/>
+                }
+            </div>
+        </header>
+    )
+}
+
+
+type HeaderPropsType = {
+    isAuth: boolean
+    userName: string
+    avatar: string
 }
