0f93ceb200048909733c2e0534cf2b8fa4656ccd
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-07-24 16:48:37 +0300

    login/logout feature

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,6 @@
 import {RegistrationActionsType} from "../pages/registration/registrationReducer";
 import {ProfileActionsType} from "../pages/profile/profileReducer";
+import {LoginActionsType} from "../pages/login/loginReducer";
 
 // все типы action для всего app
-export type ActionTypeForApp = RegistrationActionsType | ProfileActionsType;
+export type ActionTypeForApp = RegistrationActionsType | ProfileActionsType | LoginActionsType;
