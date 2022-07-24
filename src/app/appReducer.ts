99829cb7096ab8be4ca0669f10ba30b2ad055f72
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-07-24 16:41:50 +0300

    initApp feature

--- src/app/appReducer.ts
+++ src/app/appReducer.ts
@@ -1,18 +1,34 @@
+import {AppThunkType} from "../hooks/hooks";
+import {authAPI} from "../api/api";
+import {setIsLoggedIn} from "../pages/login/loginReducer";
+
 const initState = {
-  status: 'idle' as RequestStatusType
+    status: 'idle' as RequestStatusType
 }
 export const appReducer = (state: InitStateType = initState, actions: ActionsType): InitStateType => {
-  switch (actions.type) {
-    case 'CHANGE-APP-STATUS':
-      return { ...state, status: actions.status }
-    default:
-      return state
-  }
+    switch (actions.type) {
+        case 'CHANGE-APP-STATUS':
+            return {...state, status: actions.status}
+        default:
+            return state
+    }
 }
 
 export const changeAppStatus = (status: RequestStatusType) => {
-  return { type: 'CHANGE-APP-STATUS', status } as const
+    return {type: 'CHANGE-APP-STATUS', status} as const
 }
+
+export const initializeApp = (): AppThunkType => async (dispatch) => {
+    try {
+        const auth = await authAPI.authMe();
+        dispatch(setIsLoggedIn(true));
+    } catch (e) {
+
+    } finally {
+
+    }
+}
+
 type InitStateType = typeof initState
 type ActionsType = ReturnType<typeof changeAppStatus>
 
