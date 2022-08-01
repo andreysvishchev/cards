b15c90236d8271521a355157a2d78d09fa40def3
Leo <kovzel.leonid@mail.ru>
2022-08-01 20:57:39 +0300

    Refactor: slider css

--- src/components/rangeSlider/rangeSlider.tsx
+++ src/components/rangeSlider/rangeSlider.tsx
@@ -6,10 +6,14 @@ import {resetPage, setMinMaxCount, setPacksTotalCount} from "../../pages/cards/c
 
 const RangeSlider = () => {
 
-	const dispatch = useAppDispatch();
-	const [values, setValues] = useState<number[]>([0, 110])
-	const [timerId, setTimerId] = useState<number>(0)
 	const totalCount = useAppSelector(state => state.cards.cardPacksTotalCount)
+	const minCardsCount = useAppSelector(state => state.cards.minCardsCount)
+	const maxCardsCount = useAppSelector(state => state.cards.maxCardsCount)
+
+	const dispatch = useAppDispatch();
+	const [values, setValues] = useState<number[]>([minCardsCount, maxCardsCount])
+	const [timerId, setTimerId] = useState<number>(0)
+
 
 	useEffect(() => {
 		dispatch(setPacksTotalCount(totalCount))
@@ -27,20 +31,22 @@ const RangeSlider = () => {
 	return (
         <div>
            <div>
-			   <Box sx={{width: 150}}>
-				   <Slider
-					   min={0}
-					   max={80}
-					   getAriaLabel={() => 'Temperature range'}
-					   value={values}
-					   onChange={onChangeDoubleRange}
-					   valueLabelDisplay="auto"
-
-				   />
-			   </Box>
+			   <div className="slider__title">Show packs cards</div>
+			   <div className="slider__items">
+				   <span  className="slider__min">{values[0]}</span>
+				   <Box sx={{width: 150}}>
+					   <Slider
+						   min={minCardsCount}
+						   max={maxCardsCount}
+						   getAriaLabel={() => 'Temperature range'}
+						   value={values}
+						   onChange={onChangeDoubleRange}
+						   valueLabelDisplay="auto"
+					   />
+				   </Box>
+				   <span  className="slider__max">{values[1]}</span>
+			   </div>
 			   <div>
-				   <span>MIN: {values[0]}</span>
-				   <span>MAX: {values[1]}</span>
 			   </div>
 		   </div>
         </div>
