187798423acbecbe6e880d3ca005a53b0a95752a
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-08-11 22:09:14 +0300

    refactor: set logout to display correct data

--- src/pages/packsList/packsReducer.ts
+++ src/pages/packsList/packsReducer.ts
@@ -2,6 +2,7 @@ import { packApi, PacksDataType, sortingMethods } from '../../api/PackApi';
 import { changeAppStatus, setError } from '../../app/appReducer';
 import { AppStateType } from '../../app/store';
 import { AppThunkType } from '../../common/types/types';
+import { setIsLoggedIn } from '../login/loginReducer';
 
 const initState = {
   user_id: undefined, // pack id
@@ -136,6 +137,7 @@ export const getPacks =
       dispatch(setPacks({ ...response.data }));
     } catch (err: any) {
       dispatch(setError(err.response.data.error));
+      dispatch(setIsLoggedIn(false));
     } finally {
       dispatch(changeAppStatus('idle'));
     }
