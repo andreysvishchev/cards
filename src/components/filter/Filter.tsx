b4112f365cf63dd82db87da5c7d636dd03cd3f23
Leo <kovzel.leonid@mail.ru>
2022-08-12 17:02:55 +0300

    feat: add query url for filter

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+
 import { useAppDispatch, useAppSelector } from '../../common/hooks/hooks';
 import {
   setPacksOfAllUsers,
@@ -9,10 +11,21 @@ import {
 
 export const Filter = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const id = useAppSelector(state => state.profile._id);
   const status = useAppSelector(state => state.app.status);
   const filter = useAppSelector(state => state.packs.params.user_id);
+
+  function useQuery() {
+    const { search } = useLocation();
+
+    return React.useMemo(() => new URLSearchParams(search), [search]);
+  }
+  const query = useQuery();
+  const currentFilter = query.get('accessory');
+
+  console.log(currentFilter);
 
   const [isActive, setIsActive] = useState(false);
 
@@ -31,34 +44,46 @@ export const Filter = () => {
     if (filter === '') setIsActive(false);
   }, [filter]);
 
+  useEffect(() => {
+    if (currentFilter === 'My') setIsActive(true);
+    else {
+      setIsActive(false);
+      navigate('/packs?accessory=All');
+    }
+  }, [currentFilter]);
+
   return (
     <div className="filter">
       <div className="filter__title">Show packs cards</div>
       <div className="filter__buttons">
-        <button
-          onClick={getMyPacksHandler}
-          disabled={status === 'loading'}
-          type="button"
-          className={
-            isActive
-              ? 'filter__button filter__button--blue'
-              : 'filter__button filter__button--light'
-          }
-        >
-          My
-        </button>
-        <button
-          onClick={getAllPacksHandler}
-          type="button"
-          disabled={status === 'loading'}
-          className={
-            isActive
-              ? 'filter__button filter__button--light'
-              : 'filter__button filter__button--blue'
-          }
-        >
-          All
-        </button>
+        <Link to="/packs?accessory=My">
+          <button
+            onClick={getMyPacksHandler}
+            disabled={status === 'loading'}
+            type="button"
+            className={
+              isActive
+                ? 'filter__button filter__button--blue'
+                : 'filter__button filter__button--light'
+            }
+          >
+            My
+          </button>
+        </Link>
+        <Link to="/packs?accessory=All">
+          <button
+            onClick={getAllPacksHandler}
+            type="button"
+            disabled={status === 'loading'}
+            className={
+              isActive
+                ? 'filter__button filter__button--light'
+                : 'filter__button filter__button--blue'
+            }
+          >
+            All
+          </button>
+        </Link>
       </div>
     </div>
   );
