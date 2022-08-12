8383e02dfa8bb793a04d9f95313bf2b4f83c2121
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-08-12 15:27:46 +0300

    refactor: deploy to gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/style/index.scss';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { App } from './app/App';
 import { store } from './app/store';
@@ -12,8 +12,8 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
 );
